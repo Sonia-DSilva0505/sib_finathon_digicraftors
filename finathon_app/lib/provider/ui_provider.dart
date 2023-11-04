aafdf872af4e6a980b8565d94335917445befd37
Sonia D'Silva <sonia.dsilva.2003@gmail.com>
2023-11-04 18:40:20 +0530

    survey

--- finathon_app/lib/provider/ui_provider.dart
+++ finathon_app/lib/provider/ui_provider.dart
@@ -2,7 +2,7 @@ import 'package:finathon_app/utils/icon_text.dart';
 import 'package:flutter/material.dart';
 
 class UIProvider extends ChangeNotifier {
-  final most_used_feature = [
+  final mostUsedFeature = [
     'transaction_imps',
     'transaction_own_acc',
     'transaction_neft',
@@ -14,36 +14,36 @@ class UIProvider extends ChangeNotifier {
   //feature map for ['fd_open', 'kyc_update', 'FOR_add_beneficiary', 'FOR_send_money', 'priority_banking_request', 'mail_id_update', 'transaction_imps', 'transaction_neft', 'fast_tag', 'transaction_own_acc', 'billpay_recharge', 'fsld_open', 'rd_opening', 'gold_loan_renew', 'apply_apy', 'app_login']
 
   final featureComponentMap = {
-    'transaction_own_acc': IconText(
+    'transaction_own_acc': const IconText(
         iconPath: "images/icons/transaction_own_acc.png",
         text: "Transaction Own Account"),
-    'transaction_imps': IconText(
+    'transaction_imps': const IconText(
         iconPath: "images/icons/transaction_imps.png",
         text: "Transaction IMPS"),
-    'transaction_neft': IconText(
+    'transaction_neft': const IconText(
         iconPath: "images/icons/transaction_neft.png",
         text: "Transaction NEFT"),
-    'gold_loan_renew': IconText(
+    'gold_loan_renew': const IconText(
         iconPath: "images/icons/gold_loan_renew.png",
         text: "Gold Loan Renewal"),
-    'fd_open': IconText(
+    'fd_open': const IconText(
         iconPath: "images/icons/fd_open.png", text: "Open Fixed Deposit"),
     'kyc_update':
-        IconText(iconPath: "images/icons/kyc_update.png", text: "Update KYC"),
-    'FOR_add_beneficiary': IconText(
+        const IconText(iconPath: "images/icons/kyc_update.png", text: "Update KYC"),
+    'FOR_add_beneficiary': const IconText(
         iconPath: "images/icons/add_beneficiary.png", text: "Add Beneficiary"),
-    'FOR_send_money': IconText(
+    'FOR_send_money': const IconText(
         iconPath: "images/icons/transaction_own_acc.png", text: "Send Money"),
-    'priority_banking_request': IconText(
+    'priority_banking_request': const IconText(
         iconPath: "images/icons/transaction_own_acc.png",
         text: "Priority Banking Request"),
-    'mail_id_update': IconText(
+    'mail_id_update': const IconText(
         iconPath: "images/icons/kyc_update.png", text: "Update Email ID"),
     'fast_tag':
-        IconText(iconPath: "images/icons/fast_tag.png", text: "Fast Tag"),
-    'rd_opening': IconText(
+        const IconText(iconPath: "images/icons/fast_tag.png", text: "Fast Tag"),
+    'rd_opening': const IconText(
         iconPath: "images/icons/kyc_update.png", text: "Open RD Account"),
-    'apply_apy': IconText(
+    'apply_apy': const IconText(
         iconPath: "images/icons/kyc_update.png", text: "Apply APY Scheme"),
   };
 
@@ -51,7 +51,7 @@ class UIProvider extends ChangeNotifier {
   final iconList = [];
 
   UIProvider() {
-    for (var feature in most_used_feature) {
+    for (var feature in mostUsedFeature) {
       iconList.add(featureComponentMap[feature]);
     }
   }
